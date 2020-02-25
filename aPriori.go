fc84a924f5eb1b9b61eb0e51334dc30a9ce653fb
José C. Ureña <20160138@ce.pucmm.edu.do>
2020-02-25 14:11:35 -0400

    comentarios agregados

--- aPriori.go
+++ aPriori.go
@@ -13,6 +13,7 @@ type rule struct{
 	precedent  []string
 	confidence float64
 }
+//Prototipos para usar la funcion sort integrada en el slice de reglas
 type confSort []rule
 func (a confSort) Len() int           { return len(a) }
 func (a confSort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
@@ -21,6 +22,7 @@ type lenSort []rule
 func (a lenSort) Len() int           { return len(a) }
 func (a lenSort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a lenSort) Less(i, j int) bool { return (len(a[i].antecedent)+len(a[i].precedent)) < (len(a[j].antecedent)+len(a[j].precedent)) }
+
 func Find(slice []string, val string) (int, bool) {
 	for index, item := range slice {
 		if item == val {
@@ -29,6 +31,7 @@ func Find(slice []string, val string) (int, bool) {
 	}
 	return -1,false
 }
+//Devuelve los conjuntos de tamaño sizeComb dado un arreglo de itemes slice
 func NCombSlices(slice []string, sizeComb uint64,sizeSlice uint64) [][]string{
 	var combs [][]string
 	var comb []string = make([]string,sizeComb)
@@ -49,6 +52,7 @@ func NCombSlice(allCombs *[][]string,input []string, index uint64,output []strin
 	NCombSlice(allCombs,input,index+1,output,sizeComb,i+1)
 	NCombSlice(allCombs,input,index,output,sizeComb,i+1)
 }
+//Verifica si los elementos de sliceHijo estan en slicePadre
 func sliceInOther(slicePadre []string, sliceHijo []string) bool{
 	padreMap := make(map[string]int)
 	hijoMap := make(map[string]int)
@@ -75,6 +79,7 @@ func SoporteConjunto (conjunto []string,transacciones [][]string,cantTrans uint6
 	return cant
 }
 func main(){
+	//El archivo con las transacciones, el soporte minimo y la confianza minima son recibidos como 1er, 2do y 3er argumentos respectivamente al llamar al script.
 	file,err := os.Open(os.Args[1])
 	if err != nil{
 		log.Fatal(err)
@@ -86,6 +91,7 @@ func main(){
 	var formFile []([]string)
 	var transactions uint64 = 0
 	scan := bufio.NewScanner(file)
+	//Se sacan las transacciones al arreglo de strings formFile, tomando cada linea del archivo en formLine, y se cuenta la cantidad de transacciones
 	for scan.Scan(){
 		newLine := strings.Split(scan.Text()," ")
 		var formLine []string
@@ -93,6 +99,7 @@ func main(){
 		for _, element := range newLine{
 			found=false
 			formLine = append(formLine,element)
+			//Se van guardando cada elemento nuevo en un arreglo con todos los elementos existentes en una transaccion, de ya estar no se vuelven a guardar
 			for _,e:=range allElements{
 				if e==element{
 					found=true
@@ -106,6 +113,7 @@ func main(){
 		formFile=append(formFile,formLine)
 		transactions++
 	}
+	//Se buscan los elementos que cumplen con el minimo soporte para formar los datasets frecuentes
 	fmt.Println("Elementos que cumplen con el minimo soporte: ")
 	var passed []string
 	var maxSup uint64
@@ -125,12 +133,14 @@ func main(){
 		}
 		fmt.Printf("Soporte para %v: %d => %s\n", e, supC,strSup)
 	}
+	//Se comienzan a formar datasets frecuentes a partir de los elementos que cumplen con el soporte minimo, desde un tamaño de 2 hasta que el soporte minimo no sea cumplido
 	var combSize uint64 = 2
 	var allPassed [][]string
 	for _,e:=range passed{
 		allPassed = append(allPassed,[]string{e})
 	}
 	sort.Strings(passed)
+	//maxSup es el soporte maximo alcanzado por la ultima cantidad de itemes para los datasets
 	for(maxSup>=minSup){
 		newCombs:=NCombSlices(passed,combSize,uint64(len(passed)))
 		passed=[]string{}
@@ -157,6 +167,7 @@ func main(){
 		combSize++
 	}
 	var rules []rule
+	//No es necesario, pero por precaucion se remueven itemsets malformados en los pasos anteriores (vacios y con repeticiones)
 	for ind,element := range allPassed{
 		if len(element)==0 || (len(element)>1 && (strings.Compare(element[0],element[1])==0)) {
 			allPassed=append(allPassed[:ind], allPassed[ind+1:]...)
@@ -164,18 +175,24 @@ func main(){
 	}
 	for _,elm := range allPassed{
 		if len(elm)>1 && strings.Compare(elm[0],elm[1])!=0{
+			//De todos los itemsets sacados, se toman los que sean de 2 o más elementos, y se sacan subconjuntos de estos para antecedentes y procedentes
 			var k uint64=1
+			//se hacen subconjuntos para los antecedentes comenzando desde 1 hasta la cantidad de elementos -1 de forma que quede al menos un elemento como precedente
 			for (k<=uint64(len(elm)-1)){
 				antecedentes:=NCombSlices(elm,k,uint64(len(elm)))
 				for _,ant:= range antecedentes{
+					//se hace una copia del itemset original en cada iteracion de antecedente
 					itemset:=append([]string(nil),elm...)
 					for _,item:=range ant{
+						//se remueven los items que forman parte del antecedente
 						if indexReal,t:=Find(itemset,item);t{
 							itemset = append(itemset[:indexReal],itemset[indexReal+1:]...)
 						}
 					}
 					if len(itemset)>0{
+						//Se crea una nueva regla con el antecedente y los itemes restantes del itemset
 						newRule:=rule{ant,itemset,(float64(SoporteConjunto(elm,formFile,transactions))/float64(SoporteConjunto(ant,formFile,transactions)))}
+						//Si la regla cumple con la minima confianza, se añade al arreglo de reglas
 						if newRule.confidence>=minConf{
 							rules=append(rules,newRule)
 						}
@@ -186,8 +203,10 @@ func main(){
 			}
 		}
 	}
+	//Se organiza el arreglo por confianza y cantidad de elementos
 	sort.Sort(confSort(rules))
 	sort.Sort(lenSort(rules))
+	//Se imprime el arreglo de reglas, la opcion %+v nos enseñara los nombres de cada atriuto de la estructura definida anteriormente
 	for _,e:=range rules{
 		fmt.Printf("%+v\n",e)
 	}
